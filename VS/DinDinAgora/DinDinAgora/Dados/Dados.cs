1ec15c9a80896a2fdf4331abac3488409a2e00e0
Sara Pinheiro <sarapmc@hotmail.com>
2022-05-25 19:33:03 -0300

    v4

--- VS/DinDinAgora/DinDinAgora/Dados/Dados.cs
+++ VS/DinDinAgora/DinDinAgora/Dados/Dados.cs
@@ -134,10 +134,11 @@ namespace DinDinAgora.Dados
 
         public DataTable ListarRecomendacoes(int cliente, string tipo)
         {
-            string sql = @"SELECT P.TIPO, P.DESCRICAO, P.VALOR, P.STATUS, " + tipo + @"
+            string sql = @"SELECT CD.NOME, P.TIPO, P.DESCRICAO, P.STATUS, " + tipo + @"
                            FROM DINDINAGORA.SIMILARIDADE S
                                 INNER JOIN DINDINAGORA.TRANSACAO T ON T.ID_CLIENTE = S.ID_CLIENTE_DESTINO
                                 INNER JOIN DINDINAGORA.PRODUTO P ON P.ID = T.ID_PRODUTO
+                                INNER JOIN DINDINAGORA.CLIENTE CD ON CD.ID = S.ID_CLIENTE_DESTINO
                             WHERE S.ID_CLIENTE_ORIGEM = " + cliente.ToString() + @"
                               AND " + tipo + @" > 0
                               AND NOT EXISTS(SELECT * FROM DINDINAGORA.TRANSACAO T2 WHERE T2.ID_CLIENTE = S.ID_CLIENTE_ORIGEM AND T2.ID_PRODUTO = T.ID_PRODUTO)
